cc5026b5cee44dc55e82742767839201bf00c2f6
Anna Burley <anna.burley3@gmail.com>
2016-10-08 23:11:06 +0100

    Fixed style of the getMessage function

--- src/js/check.js
+++ src/js/check.js
@@ -3,27 +3,28 @@
  */
 function getMessage(a,b){
   if (typeof a === 'boolean'){
+    var pictureReturn;
     if (a) {
-      var pictureReturn = "Переданное GIF-изображение анимировано и содержит " + b + " кадров";
+      pictureReturn = 'Переданное GIF-изображение анимировано и содержит ' + b + ' кадров';
     } else {
-      var pictureReturn = "Переданное GIF-изображение не анимировано";
+      pictureReturn = 'Переданное GIF-изображение не анимировано';
     }
   } else if (typeof a === 'number'){
-    var pictureReturn = "Переданное SVG-изображение содержит " + a + " объектов и " + (b * 4) + " атрибутов";
+    pictureReturn = 'Переданное SVG-изображение содержит ' + a + ' объектов и ' + (b * 4) + ' атрибутов';
   } else if (Array.isArray(a)) {
     var amountOfRedPoints = 0;
     for (var i = 0; i < a.length; i++) {
       amountOfRedPoints += a[i];
     }
-    var pictureReturn =  "Количество красных точек во всех строчках изображения: " + amountOfRedPoints;
+    pictureReturn =  'Количество красных точек во всех строчках изображения: ' + amountOfRedPoints;
   } else if (Array.isArray(a) && Array.isArray(b)){
     var artifactsSquare = 0;
     for(i = 0; i < a.length; i++){
       artifactsSquare += (a[i] * b[i]);
     }
-    var pictureReturn =  "Общая площадь артефактов сжатия: " + artifactsSquare +  "пикселей";
+    pictureReturn =  'Общая площадь артефактов сжатия: ' + artifactsSquare +  'пикселей';
   } else {
-    var pictureReturn = "Переданы некорректные данные";
+    pictureReturn = 'Переданы некорректные данные';
   }
   return pictureReturn;
 }
