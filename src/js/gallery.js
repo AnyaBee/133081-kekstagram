94c0a200149b06a711241429cb793430b6c5e914
Anna Burley <anna.burley3@gmail.com>
2016-11-14 20:28:12 +0000

    Fixed this/self ingallery file

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,46 +1,44 @@
 'use strict';
 module.exports = function() {
   var Gallery = function() {
-    var self = this;
     this.pictures = [];
     this.activePicture = 0;
     this.overlay = document.querySelector('gallery-overlay');
     this.overlayClose = document.querySelector('gallery-overlay-close');
     this.overlayImage = document.querySelector('gallery-overlay-image');
+  };
+  Gallery.prototype.setPictures = function(pictures) {
+    this.pictures = pictures;
+  };
 
-    Gallery.prototype.setPictures = function(pictures) {
-      self.pictures = pictures;
+  Gallery.prototype.show = function(number) {
+    var self = this;
+    this.overlayClose.onclick = function() {
+      self.hide();
     };
 
-    Gallery.prototype.show = function(number) {
-      self.overlayClose.onclick = function() {
-        self.hide();
-      };
-
-      self.overlayImage.onclick = function(evt) {
-        if (evt.target === self.overlayImage) {
-          if (self.activePicture === self.pictures.length - 1) {
-            self.setActivePicture(0);
-          }
-          self.setActivePicture(self.activePicture + 1);
+    this.overlayImage.onclick = function(evt) {
+      if (evt.target === self.overlayImage) {
+        if (self.activePicture === self.pictures.length - 1) {
+          self.setActivePicture(0);
         }
-      };
-      self.overlay.classList.remove('invisible');
-      self.setActivePicture(number);
+        self.setActivePicture(self.activePicture + 1);
+      }
     };
+    self.overlay.classList.remove('invisible');
+    self.setActivePicture(number);
+  };
 
-    Gallery.prototype.hide = function() {
-      self.overlay.classList.add('invisible');
-    };
+  Gallery.prototype.hide = function() {
+    this.overlay.classList.add('invisible');
+  };
 
-    Gallery.prototype.setActivePicture = function(number) {
-      self.activePicture = number;
-      var largeImage = self.pictures[number];
-      self.overlayImage.src = largeImage.url;
-      document.querySelector('.likes-count').innerHTML = largeImage.likes;
-      document.querySelector('.comments-count').innerHTML = largeImage.comments;
-    };
-
+  Gallery.prototype.setActivePicture = function(number) {
+    this.activePicture = number;
+    var largeImage = this.pictures[number];
+    this.overlayImage.src = largeImage.url;
+    document.querySelector('.likes-count').innerHTML = largeImage.likes;
+    document.querySelector('.comments-count').innerHTML = largeImage.comments;
   };
   return new Gallery();
 };
