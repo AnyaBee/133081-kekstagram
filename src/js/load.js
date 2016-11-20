5192548284276b237dd8420bcdcfd3297c3d6ce8
Anna Burley <anna.burley3@gmail.com>
2016-11-20 13:22:26 +0000

    Removed xhr.onload from load file

--- src/js/load.js
+++ src/js/load.js
@@ -6,12 +6,12 @@
 var loadImageList = function(url, params, callback) {
   var xhr = new XMLHttpRequest();
 
-  xhr.onload = function(evt) {
-    var loadedData = JSON.parse(evt.target.response);
-    callback(loadedData);
-  };
 
   xhr.open('GET', url + '?' + 'from=' + params.from + '&to=' + params.to + '&filter=' + params.filter);
+  xhr.addEventListener('load', function(evt) {
+    var loadedData = JSON.parse(evt.target.response);
+    callback(loadedData);
+  });
   xhr.send();
 };
 
